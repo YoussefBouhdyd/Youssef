cf39ad4569bfca145e8a34bd8ce0c463ae38a213
YoussefBouhdyd <bouhdyd2004@gmail.com>
2024-07-02 08:56:06 +0100

    fiex some bugs in from of forks

--- my tools/clip_path_genertor.py
+++ my tools/clip_path_genertor.py
@@ -1,24 +1,3 @@
-# All Control from here 
-
-# def clip_path(N:int) -> str:
-#     path = "clip-path: polygon(0 0,"
-#     patern = 100 / N
-#     X = 0
-#     Y = 0
-#     while(X != 100):
-#         X += patern
-#         if Y:
-#             Y = 0
-#         else:
-#             Y = 3
-#         path += f"{int(X)}% {Y}%,"
-#     path += "100% 100%,0% 100%)"
-#     return path
-
-# Conrol With CSS Variabls 
-
-# var(--fork-hight)
-
 def clip_path(N:int) -> str:
     """
     This function help us in creating clip path for creating the form of fork in the
@@ -29,14 +8,14 @@ def clip_path(N:int) -> str:
     patern = 100 / N
     X = 0
     Y = 0
-    while(X != 100):
+    while(X < 100):
         X += patern
         if Y:
             Y = 0
         else:
             Y = "var(--fork-hight)"
-        path += f"{int(X)}% {Y},"
+        path += f"{X:.2f}% {Y},"
     path += "100% 100%,0% 100%)"
     return path
 
-print(clip_path(100))
+print(clip_path(80))
